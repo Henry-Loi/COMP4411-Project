c83b9a62bd11aeddcf35e666e95de7d1bdc37e77
KOK <yungkangkok@gmail.com>
2024-05-10 14:14:16 +0800

    added bezier with wrap

---
+++ AniSkelS20/bezierCurveEvaluator.h
@@ -0,0 +1,25 @@
+#ifndef INCLUDED_BEZIER_CURVE_EVALUATOR_H
+#define INCLUDED_BEZIER_CURVE_EVALUATOR_H
+
+
+#pragma warning(disable : 4786)  
+
+#include "CurveEvaluator.h"
+
+using namespace std;
+
+class BezierCurveEvaluator : public CurveEvaluator
+{
+public:
+	void evaluateCurve(const std::vector<Point>& ptvCtrlPts,
+		std::vector<Point>& ptvEvaluatedCurvePts,
+		const float& fAniLength,
+		const bool& bWrap) const;
+	Mat4f basis = Mat4f(
+		-1.0, 3.0, -3.0, 1.0,
+		3.0, -6.0, 3.0, 0.0,
+		-3.0, 3.0, 0.0, 0.0,
+		1.0, 0.0, 0.0, 0.0);
+};
+
+#endif
